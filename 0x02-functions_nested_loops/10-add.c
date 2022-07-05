5b3c84981590be29a39d981b46b3e735eeb0d55a
Abba Magaji <abbamagaji49@gmail.com>
2022-07-05 08:48:19 -0700

    add to integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add -> to add to integers
+ */
+int add(int int)
+{
+	int n;
+
+	n = add (10,5);
+	_putchar("%d\n", n);
+	
+	return (0);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,4 +3,24 @@
 /**
  * times_table -> to print times_table from 0 to 9
  */
+void times_table(void)
+{
+	int i, j, n;
+	
+	for (i = 0; i <= 9; i++)
+	{
+		for(j = 0; j <= n; j++)
+		{
+			if(j <= n-0)
+			{	
+				_putchar("%dx%d = %d, ",j,i,i*j);
+			}
+			else
+			{
+				_putchar("%dx%d = %d", j,i,i*j);
+			}
+			_putchar("\n");
 
+		}
+	}
+}
