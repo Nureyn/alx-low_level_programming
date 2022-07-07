5b07a69c42e569bff089d4e86b338be10dadf780
Abba Magaji <abbamagaji49@gmail.com>
2022-07-07 04:32:30 -0700

    fix

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_diagonal -> printing line
+* @n: integer params
+*/
+void print_diagonal(int n)
+{
+	int x;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x <= n; x++)
+		{
+			for (i = 0; i <= x; i++)
+				_putchat(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+
+}
